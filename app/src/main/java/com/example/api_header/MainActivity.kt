4d5130a0eaf7e92390e71ac303f4883e7aaf212d
Derian2426 <victor.chun2017@uteq.edu.ec>
2022-06-28 11:26:47 -0500

    USO DE JSON API RESTFUL CON PARÁMETROS EN EL HEADER USANDO VOLLEY

---
+++ app/src/main/java/com/example/api_header/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.api_header
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import kotlin.math.log
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun call_API(view: View){
+
+        val txt=findViewById<TextView>(R.id.txt_respuesta)
+        val txt_metal=findViewById<TextView>(R.id.txtMetal)
+        val url = "https://www.goldapi.io/api/XAU/USD"
+
+        val cola = Volley.newRequestQueue(this)
+        val stringRequest = object: StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                val json=Gson()
+                val metal:Metal=json.fromJson(response,Metal::class.java)
+                txt.text= "${metal.metal}"+" "+"${metal.currency}"+" "+"${metal.price}\n"+
+                        "Precio en gramos de 24k :${metal.currency}"+" "+"${metal.price_gram_24k}\n"+
+                        "Precio en gramos de 22k :${metal.currency}"+" "+"${metal.price_gram_22k}\n"+
+                        "Precio en gramos de 21k :${metal.currency}"+" "+"${metal.price_gram_21k}\n"+
+                        "Precio en gramos de 20k :${metal.currency}"+" "+"${metal.price_gram_20k}\n"+
+                        "Precio en gramos de 18k :${metal.currency}"+" "+"${metal.price_gram_18k}\n"
+                txt_metal.text="${metal.metal}"
+            },
+            Response.ErrorListener { txt.text="Ocurrio un error" })
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["x-access-token"] = "goldapi-jicyctl4x01ott-io"
+                return headers
+            }
+        }
+
+        cola.add(stringRequest)
+    }
+}
---
+++ app/src/main/java/com/example/api_header/Metal.kt
@@ -0,0 +1,25 @@
+package com.example.api_header
+
+class Metal {
+    var timestamp=1656367848
+    var metal=""
+    var currency =""
+    var exchange =""
+    var symbol="FOREXCOM:XAUUSD"
+    var prev_close_price=1820.83
+    var open_price=1822.65
+    var low_price=1821.79
+    var high_price=1822.97
+    var open_time=1656374400
+    var price=1822.05
+    var ch=1.22
+    var chp=0.07
+    var ask=1822.53
+    var bid=1821.6
+    var price_gram_24k=58.5803
+    var price_gram_22k=53.6986
+    var price_gram_21k=51.2577
+    var price_gram_20k=48.8169
+    var price_gram_18k=43.9352
+
+}

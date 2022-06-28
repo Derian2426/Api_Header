0c61ce5b10d81d868ce11da1f4f66052a8f9732b
Derian2426 <victor.chun2017@uteq.edu.ec>
2022-06-28 12:04:49 -0500

    Cambios

--- app/src/main/java/com/example/api_header/Metal.kt
+++ app/src/main/java/com/example/api_header/Metal.kt
@@ -22,4 +22,50 @@ class Metal {
     var price_gram_20k:Double=0.0
     var price_gram_18k:Double=0.0
 
+    constructor(
+        timestamp: Int,
+        metal: String,
+        currency: String,
+        exchange: String,
+        symbol: String,
+        prev_close_price: Double,
+        open_price: Double,
+        low_price: Double,
+        high_price: Double,
+        open_time: Long,
+        price: Double,
+        ch: Double,
+        chp: Double,
+        ask: Double,
+        bid: Double,
+        price_gram_24k: Double,
+        price_gram_22k: Double,
+        price_gram_21k: Double,
+        price_gram_20k: Double,
+        price_gram_18k: Double
+    ) {
+        this.timestamp = timestamp
+        this.metal = metal
+        this.currency = currency
+        this.exchange = exchange
+        this.symbol = symbol
+        this.prev_close_price = prev_close_price
+        this.open_price = open_price
+        this.low_price = low_price
+        this.high_price = high_price
+        this.open_time = open_time
+        this.price = price
+        this.ch = ch
+        this.chp = chp
+        this.ask = ask
+        this.bid = bid
+        this.price_gram_24k = price_gram_24k
+        this.price_gram_22k = price_gram_22k
+        this.price_gram_21k = price_gram_21k
+        this.price_gram_20k = price_gram_20k
+        this.price_gram_18k = price_gram_18k
+    }
+
+    constructor()
+
 }
